c8fd1d4b67f56a490128f9aa5edc1144255592fd
rowenarono95 <rowenarono@gmail.com>
2020-09-14 12:51:18 +0300

    Created class Sources

--- app/models.py
+++ app/models.py
@@ -14,20 +14,20 @@ class Sources:
 
 
     
-class Articles:
-    '''
-    Articles class to define Articles Objects
-    '''
+# class Articles:
+#     '''
+#     Articles class to define Articles Objects
+#     '''
 
-    def __init__(self,id,author,title,description,url,urlToImage,publishedAt,content):
-        self.id = id
-        self.author = author
-        self.title = title
-        self.description = description
-        self.url = url
-        self.urlToImage = urlToImage
-        self.publishedAt = publishedAt
-        self.content = content
+#     def __init__(self,id,author,title,description,url,urlToImage,publishedAt,content):
+#         self.id = id
+#         self.author = author
+#         self.title = title
+#         self.description = description
+#         self.url = url
+#         self.urlToImage = urlToImage
+#         self.publishedAt = publishedAt
+#         self.content = content
 
 
     
